f60a7dfae37436f9843fa1e953fde35b3603c3d7
黃詠潔 <jessicahuang29@gmail.com>
2022-11-04 01:54:32 -0500

    first commit

--- app/src/main/java/com/example/dailyart/MainActivity.java
+++ app/src/main/java/com/example/dailyart/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.dailyart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button profileButton;
+    private Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileButton = (Button) findViewById(R.id.profile_button);
+        profileButton.setOnClickListener(this);
+        settingsButton = (Button) findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        if(v.getId() == R.id.profile_button){
+            intent = new Intent(this, ProfileActivity.class);
+        }
+        else if(v.getId() == R.id.settings_button){
+            intent = new Intent(this, SettingsActivity.class);
+        }
+        startActivity(intent);
+    }
+
 }
