fc957ea34b90fbb36252ad6ba17906aad0c0c632
黃詠潔 <jessicahuang29@gmail.com>
2022-11-11 15:09:20 -0600

    generate ID

--- app/src/main/java/com/example/dailyart/MainActivity.java
+++ app/src/main/java/com/example/dailyart/MainActivity.java
@@ -11,10 +11,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences sharedPref;
     private String userID;
     private TextView welcomeView;
+    private InputStream dbIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +30,48 @@ public class MainActivity extends AppCompatActivity {
 
         sharedPref = getSharedPreferences("CurrentUserID", MODE_PRIVATE);
         userID = sharedPref.getString("UserID", "");
-        if (userID.length() != 0) {
-            welcomeView = (TextView) findViewById(R.id.welcome_view);
-            welcomeView.setText("Welcome, " + userID + "!");
-        }
-        else {
+
+        if (userID.length() == 0) {
+            generateID();
             AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-            dialogBuilder.setMessage("Welcome! Your User ID is xxx. You can share the ID to others to log in to your page. You can find this ID on the Profile page.")
+            dialogBuilder.setMessage("Welcome! Your User ID is " + userID + ". You can share the ID to others to log in to your page. You can find this ID on the Profile page.")
                     .setTitle("Your User ID")
                     .setCancelable(false)
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
+                            SharedPreferences.Editor editor = sharedPref.edit();
+                            editor.putString("UserID", String.valueOf(userID));
+                            editor.apply();
                             dialogInterface.cancel();
                         }
                     });
             AlertDialog dialog = dialogBuilder.create();
             dialog.show();
+        }
+
+        welcomeView = (TextView) findViewById(R.id.welcome_view);
+        welcomeView.setText("Welcome, " + userID + "!");
+    }
+
+    public void generateID() {
+        try {
+            dbIS = getAssets().open("userDB.json");
+            int size = dbIS.available();
+            byte[] buffer = new byte[size];
+            dbIS.read(buffer);
+            dbIS.close();
+            String json = new String(buffer, "UTF-8");
+
+            JSONObject obj = new JSONObject(json);
+            int userCount = obj.getInt("userCount");
+            userID = Integer.toString(userCount + 1);
+
+            obj.put("userCount", userCount + 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 
