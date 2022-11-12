5fa270be43eab61cd31e740710f0544aa32f7e56
yungchieh <jessicahuang29@gmail.com>
2022-11-12 14:44:30 -0600

    generate ID and main layout

--- app/src/main/java/com/example/dailyart/LoginActivity.java
+++ app/src/main/java/com/example/dailyart/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.dailyart;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
--- app/src/main/java/com/example/dailyart/MainActivity.java
+++ app/src/main/java/com/example/dailyart/MainActivity.java
@@ -11,17 +11,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences sharedPref;
     private String userID;
     private TextView welcomeView;
-    private InputStream dbIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +36,9 @@ public class MainActivity extends AppCompatActivity {
         sharedPref = getSharedPreferences("CurrentUserID", MODE_PRIVATE);
         userID = sharedPref.getString("UserID", "");
 
+        // new user
         if (userID.length() == 0) {
-            generateID();
+            this.generateID();
             AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
             dialogBuilder.setMessage("Welcome! Your User ID is " + userID + ". You can share the ID to others to log in to your page. You can find this ID on the Profile page.")
                     .setTitle("Your User ID")
@@ -55,19 +61,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void generateID() {
+        File file = new File(getBaseContext().getFilesDir(), "usersDB.json");
+        String outputString = "";
         try {
-            dbIS = getAssets().open("userDB.json");
-            int size = dbIS.available();
-            byte[] buffer = new byte[size];
-            dbIS.read(buffer);
-            dbIS.close();
-            String json = new String(buffer, "UTF-8");
+            // exists other users
+            if(file.exists()) {
+                FileReader fileReader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = bufferedReader.readLine();
+                while (line != null) {
+                    stringBuilder.append(line).append("\n");
+                    line = bufferedReader.readLine();
+                }
+                bufferedReader.close();
+                String response = stringBuilder.toString();
 
-            JSONObject obj = new JSONObject(json);
-            int userCount = obj.getInt("userCount");
-            userID = Integer.toString(userCount + 1);
+                JSONObject jsonObject = new JSONObject(response);
+                int usersCount = jsonObject.getInt("usersCount");
+                userID = Integer.toString(usersCount + 1);
+                jsonObject.put("usersCount", usersCount + 1);
 
-            obj.put("userCount", userCount + 1);
+                JSONArray usersArray = jsonObject.getJSONArray("users");
+                JSONObject newUserObject = new JSONObject();
+                newUserObject.put("userID", userID);
+                newUserObject.put("userName", "");
+                usersArray.put(newUserObject);
+
+                outputString = jsonObject.toString();
+            }
+
+            // first user of the app
+            else {
+                userID = new String("1");
+
+                JSONObject jsonObject = new JSONObject();
+                JSONArray usersArray = new JSONArray();
+                JSONObject newUserObject = new JSONObject();
+                newUserObject.put("userID", userID);
+                newUserObject.put("userName", "");
+                usersArray.put(newUserObject);
+                jsonObject.put("users", usersArray);
+                jsonObject.put("usersCount", 1);
+
+                outputString = jsonObject.toString();
+            }
+
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(outputString);
+        bufferedWriter.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (JSONException e) {
